8f737b6a052d1f655a0864c3e7f39fa5781ac287
Mohammed <mohammed@fluxfullcircle.com>
2025-09-08 09:48:10 +0200

    yelementary falise

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     experimental: {
     turbo: process.env.NODE_ENV === 'development' ? {} : undefined
-  }
+  },
+  telemetry: false,
 };
 
 export default nextConfig;
